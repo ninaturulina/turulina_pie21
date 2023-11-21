e4e6b2803ccee9164c6f4ef3d054f4df4faf352b
ninaturulina <151495361+ninaturulina@users.noreply.github.com>
2023-11-21 10:34:59 +0700

    Add files via upload

---
+++ square.js
@@ -0,0 +1,22 @@
+function f(x) {
+    let a = Math.sqrt(x);
+    if (a * a === x) {
+        return 'Да, это число является квадратом числа ' + a;
+    }
+    else {
+        return 'Нет, это число не является квадратом';
+    }
+}
+
+let myX = prompt('Введите x:');
+if (parseInt(myX) != NaN) {
+    myX = parseInt(myX);
+    if (myX < 0) {
+        alert('Квадрат из отрицательного числа не извлекается');
+    }
+    else{
+        let check = f(myX);
+        alert(check);
+    }
+
+}
